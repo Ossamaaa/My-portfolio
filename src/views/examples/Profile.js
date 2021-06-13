5d526762f45a180565b300a2119f27918e145fd5
osama al-shararbeh <osskhsh@gmail.com>
2021-06-13 09:12:46 +0300

    added my portf

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -1,19 +1,13 @@
 /*!
-
 =========================================================
 * Argon Design System React - v1.1.0
 =========================================================
-
 * Product Page: https://www.creative-tim.com/product/argon-design-system-react
 * Copyright 2020 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/main/LICENSE.md)
-
 * Coded by Creative Tim
-
 =========================================================
-
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
 import React from "react";
 
@@ -23,7 +17,7 @@ import { Button, Card, Container, Row, Col } from "reactstrap";
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import profile from "assets/img/theme/Osama.jpg";
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -124,31 +118,39 @@ class Profile extends React.Component {
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Osama Al-shararbeh{" "}
+                      <span className="font-weight-light">, 24</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Az-zarqa, Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Web developer - ASAC
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                     Al-albyet University 
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                         I graduated in Accounting from
+Al al-Bayt University , 
+                         I am very excited to learn about web development and become a professional developer.
+                        </p>
+                        <p>
+                        Projects 
+            - jordan-wonders
+ | April 2021 |  
+
+            -It's all about ENCOURAGING DOMESTIC TOURISM IN JORDAN.
+
+
+          - Built with JavaScript, HTML, CSS.
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
